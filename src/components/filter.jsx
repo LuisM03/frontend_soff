a1cad2f85729bf551b5de827149950dc11d5ca66
evelynmontoyav <evemontoya10@ielasnieves.edu.co>
2023-03-02 23:45:14 -0500

    new filter

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,4 +1,3 @@
-import "./styles/style-filter.css"
 import Lupa from "./icons/lupa"
 import Vector from "./icons/vector"
 import Flechabaja from "./icons/flecha-abajo"
@@ -8,26 +7,26 @@ export default function Filter(){
     return(
         <div className="flex w-full p-3">
             <form className="flex">
-                <div className="flex filtro p-4 items-center">
+                <div className="flex filtro p-4 items-center text-[#757575] rounded h-12 border border-solid border-1 border-[#757575]">
                     <Lupa/>
-                    <input id="" type="text" placeholder="Filtrar busqueda..." className="ml-2"/>
+                    <input id="" type="text"  placeholder="Filtrar busqueda..." className="ml-2"/>
                 </div>
 
-                <div className="flex filtro p-4 items-center ml-4">
+                <div className="flex filtro p-4 items-center ml-4 text-[#757575] rounded h-12 border border-solid border-1 border-[#757575]">
                     <Vector/>
                     <input id="" type="text" placeholder="Ordenar por" className="ml-2"/>
                     <Flechabaja/>
                 </div>
             </form>
 
-            <div className="reporte p-4 flex items-center ml-4">
+            <div className="p-4 flex items-center ml-4 text-[#0453F4] rounded h-12 border border-solid border-1 border-[#0453F4]">
                 <button>Generar reporte de ventas</button>
             </div>
 
-            <div className="boton p-4 flex ml-auto">
+            <div className=" p-4 flex ml-auto bg-[#0453F4] text-white rounded h-12">
                 <button className="flex items-center">
                     <Flechaizquierda/>
-                    <p className="ml-2">Regresar a vender</p>
+                    <label className="ml-2">Regresar a vender</label>
                 </button> 
             </div>
         </div>
