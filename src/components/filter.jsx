2855879d1bfc25a5bdd24ccdda06d83b78715058
LuisM03 <luisdavidmontes123@gmail.com>
2023-03-04 23:09:43 -0500

    Nuevos cambios

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -4,32 +4,28 @@ import Flechaizquierda from "./icons/flecha-izquierda"
 
 export default function Filter(props){
     return(
-        <div className="flex w-full p-3">
+        <div className="flex max-w-[1200px] mx-auto mb-4">
             <form className="flex">
-                <div className="flex p-4 items-center text-[#757575] rounded h-12 border border-solid border-1 border-[#757575]">
+                <div className="flex p-4 items-center text-[#757575] rounded h-12 border border-solid border-[#757575] bg-[#F8F8F8]">
                     <Lupa/>
-                    <input id="" type="text"  placeholder="Filtrar busqueda..." className="ml-2"/>
+                    <input id="" type="text"  placeholder="Filtrar busqueda..." className="ml-2 bg-[#F8F8F8]"/>
                 </div>
 
-                <div className="flex p-4 items-center ml-4 text-[#757575] rounded h-12 border border-solid border-1 border-[#757575]">
+                <div className="flex p-4 items-center ml-4 text-[#757575] rounded h-12 border border-solid border-[#757575] bg-[#F8F8F8]">
                     <Vector/>
                     <label className="ml-2">Ordenar por</label>
-                    <select name="" id="">
-                        <option value="">Fecha</option>
-                        <option value="">N° de ordenes</option>
-                        <option value="">Estado</option>
-                    </select>
+                    
                 </div>
             </form>
 
-            <div className="p-4 flex items-center ml-4 text-[#0453F4] rounded h-12 border border-solid border-1 border-[#0453F4]">
+            <div className="p-4 flex items-center ml-4 text-[#0453F4] rounded h-12 border border-solid border-1 border-[#0453F4] bg-white">
                 <button>Generar reporte de {props.modulo}</button>
             </div>
 
             <div className=" p-4 flex ml-auto bg-[#0453F4] text-white rounded h-12">
                 <button className="flex items-center">
                     <Flechaizquierda/>
-                    <label className="ml-2">{props.boton_agregar}</label>
+                    <label className="ml-2">{props.name}</label>
                 </button> 
             </div>
         </div>
--- src/views/providers.jsx
+++ src/views/providers.jsx
@@ -1,18 +1,20 @@
 import Sidebar from "../components/sidebar/sidebar"
 import Tabla from "../components/tabla"
 import Filter from "../components/filter"
+import Footer from "../components/login_components/footer_login"
 import Cuerpo from "../components/cuerpo"
 import Cabecera from "../components/cabecera"
 
 export default function Providers(){
     return(
-        <div className="flex h-screen w-screen border-2 border-black " >
+        <div className="flex min-h-screen w-full bg-[#F2F2F2]" >
           <Sidebar />
-          <div className="inline-block  w-full h-full relative ">
-            <Cabecera/>
-            <Cuerpo/>
-            <Filter/>
+          <div className="inline-block w-full relative ">
+            <Cabecera username="José Luis" lastname="Guzman"/>
+            <Cuerpo modulo="Proveedores" />
+            <Filter name="Agregar proveedor" modulo="proveedores"/>
             <Tabla/>
+            <Footer />
           </div> 
         </div>
     )
